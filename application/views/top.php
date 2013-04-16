3f4e2a5e822cae5bf1bd60fda1a4684eb6e7f3f8
yatrahelpline <yathrahelpline@gmail.com>
2013-04-16 10:43:11 -0700

    top fiile included

---
+++ application/views/top.php
@@ -0,0 +1,12 @@
+<h1 id="head">Yatrahelpline</h1>
+	<ul id="navigation">
+			<li><a href="spot">Spot</a></li>
+			<li><a href="homestay">Homestays</a></li>
+			<li><a href="hotel">Hotels</a></li>
+                        <li><a href="gallery">Gallery</a></li>
+                        <li><a href="personalities">Personalities</a></li>
+                        <li><a href="#">Relation</a></li>
+                        <li><a href="#">Type</a></li>
+                     
+		</ul>
+
