979023e1f36b96122b1afaa934c1ed68bb1424cd
yatrahelpline <yathrahelpline@gmail.com>
2013-04-17 12:25:15 -0700

    Top menu files link updation completed

--- application/views/top.php
+++ application/views/top.php
@@ -1,10 +1,15 @@
-<h1 id="head">Yatrahelpline</h1>
+
+<h1 id="head">
+    <a href="<?php echo base_url('index.php/dashboard');  ?>" style="text-decoration: none;color:white">
+    Yatrahelpline
+    </a>
+</h1>
 	<ul id="navigation">
-			<li><a href="spot">Spot</a></li>
-			<li><a href="homestay">Homestays</a></li>
-			<li><a href="hotel">Hotels</a></li>
-                        <li><a href="gallery">Gallery</a></li>
-                        <li><a href="personalities">Personalities</a></li>
+			<li><a href="<?php echo base_url('index.php/spot');  ?>">Spot</a></li>
+			<li><a href="<?php echo base_url('index.php/homestay');  ?>">Homestays</a></li>
+			<li><a href="<?php echo base_url('index.php/hotel');  ?>">Hotels</a></li>
+                        <li><a href="<?php echo base_url('index.php/gallery');  ?>">Gallery</a></li>
+                        <li><a href="<?php echo base_url('index.php/personalities');  ?>">Personalities</a></li>
                         <li><a href="#">Relation</a></li>
                         <li><a href="#">Type</a></li>
                      
