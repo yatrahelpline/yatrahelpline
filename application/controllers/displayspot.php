82f895f4205c40d201254685f81549d84fb04aae
yatrahelpline <yathrahelpline@gmail.com>
2013-04-24 11:48:32 -0700

    display spot

--- application/controllers/displayspot.php
+++ application/controllers/displayspot.php
@@ -31,6 +31,122 @@ class Displayspot extends CI_Controller {
 
 
 
+         public function edit($id=0)
+	{
+
+
+
+
+                        $this->load->helper(array('form', 'url'));
+		        $this->load->library('form_validation');
+                        $this->form_validation->set_rules('title', 'Title', 'required');
+
+                        $this->form_validation->set_rules('title', 'Title', 'required');
+                        $this->form_validation->set_rules('keyword', 'Keyword', 'required');
+                        $this->form_validation->set_rules('descr', 'Description', 'required');
+                        $this->form_validation->set_rules('data', 'Data', 'required');
+                        $this->form_validation->set_rules('relationid', 'Relation Id', 'required');
+                        $this->form_validation->set_rules('primary', 'Primary', 'required');
+
+                        $add['table']='spot';
+			$add['data']['title']           = $this->input->post('title');
+
+			$add['data']['keyword']           = $this->input->post('keyword');
+			$add['data']['descr']           = $this->input->post('descr');
+			$add['data']['data']           = $this->input->post('data');
+			$add['data']['relationid']        = $this->input->post('relationid');
+			$add['data']['primary']           = $this->input->post('primary');
+
+
+                              $data['error']='sss';
+                              $data['id']=intval($id);
+                              $data['msg']='';
+
+
+
+
+                        if ($this->form_validation->run() == FALSE)
+                            {
+
+                              $data['error']='';
+
+                            //$this->load->view('spot/content.php');
+                            }
+                            else
+                            {
+                                   //  insert($add);
+
+                              $data['error']= $this->do_upload('imageid1');
+                              $data1['error']= $this->do_upload('imageid2');
+                              $data2['error']= $this->do_upload('imageid3');
+
+
+
+                           $add['data']['imageid1']   = $data['error']['upload_data']['file_name'];
+                           $add['data']['imageid2']   = $data1['error']['upload_data']['file_name'];
+                           $add['data']['imageid3']   = $data2['error']['upload_data']['file_name'];
+
+
+
+                           
+
+
+                           $ins=insert($add);
+                           $data['msg']='';
+                           if($ins)
+                           {
+                           $data['msg']='You have Updated successfully';
+                           }
+
+                           
+
+
+                           }
+
+
+
+                           $this->load->model('displayspot_model');
+	                   $data['query']=$this->displayspot_model->spot_getedit($id);
+
+
+              $this->load->view('header.php');
+              $this->load->view('top.php');
+	      $this->load->view('spot/content.php',$data);
+              $this->load->view('footer.php');
+
+
+
+        }
+
+  function do_upload($field="")
+	{
+		$config['upload_path'] = 'uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload($field))
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			return $error;
+
+                        //echo $error;
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			return $data;
+
+                       // echo $data;
+		}
+	}
+
+
+
+
+
         
 }
 
